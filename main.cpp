699673c50cbb6e8c4fe70bdf60697c6ed2e13f74
Kevin Wu <kevinwu127@gmail.com>
2017-01-23 18:57:45 +0000

    Added and integrated config_parser to project.

--- main.cpp
+++ main.cpp
@@ -14,26 +14,50 @@
 #include <boost/bind.hpp>
 #include "server.hpp"
 
+#include "config_parser.hpp"
+
 int main(int argc, char* argv[])
 {
   try
   {
-    // Check command line arguments.
-    if (argc != 4)
+
+    // TODO: Take in one command-line argument for config file
+    if (argc != 2)
     {
-      std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
-      std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    receiver 0.0.0.0 80 .\n";
-      std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    receiver 0::0 80 .\n";
+      std::cerr << "Usage: ./server /path/to/config/file\n";
       return 1;
     }
 
-    // Initialise the server.
-    http::server::server s(argv[1], argv[2], argv[3]);
+    // Initialise the NginxConfigParser
+    NginxConfigParser *parser = new NginxConfigParser();
+    NginxConfig *out_config = new NginxConfig();
 
-    // Run the server until stopped.
-    s.run();
+    parser->Parse(argv[1], out_config);
+
+    // out_config is a list of vectors representing statements
+    // Iterate through all statements
+    // Construct a server object to hold params
+    // Pass object.params into server constructor
+    // http::server::server s(server.name,server.port, server.location);
+
+    // UNCOMMENT BELOW FOR REGULAR OPERATION
+
+    // Check command line arguments.
+    // if (argc != 4)
+    // {
+    //   std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
+    //   std::cerr << "  For IPv4, try:\n";
+    //   std::cerr << "    receiver 0.0.0.0 80 .\n";
+    //   std::cerr << "  For IPv6, try:\n";
+    //   std::cerr << "    receiver 0::0 80 .\n";
+    //   return 1;
+    // }
+    //
+    // Initialise the server.
+    // http::server::server s('0.0.0.0', argv[2], argv[3]);
+    //
+    // // Run the server until stopped.
+    // s.run();
   }
   catch (std::exception& e)
   {

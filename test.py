6081f0a64b2a861f31ac5603870c525966cee2f1
Kevin Wu <kevinwu127@gmail.com>
2017-01-31 00:32:03 +0000

    completed integration test. should be able to set up, make a request, and shut down successfully

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import subprocess
 import os
 
+# REQUIRES LIBRARY TO RUN
+import psutil
+
 def get_output_of_shell_command(cmd):
   p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
   out, err = p.communicate()
@@ -11,17 +14,36 @@ def get_output_of_shell_command(cmd):
 def run_background_process(cmd):
   os.system(cmd)
 
-
 def main():
 
+  # Make and get output
   print get_output_of_shell_command("make")
-  run_background_process("./webserver demo/example.conf &")
-  print get_output_of_shell_command("GET http://localhost:3000")
 
-  # TODO:
-  # 1. Silence output to console
-  # 2. Compare output with desired test outcome
-  # 3. Output integration test result
+  # Run the webserver in the background
+  run_background_process("./webserver demo/example.conf &")
+
+  # GET the HTTP response
+  http_req = "GET http://localhost:3000"
+  http_res = get_output_of_shell_command(http_req)
+
+  # Print for clarity
+  print "HTTP request: \n"
+  print http_req + "\n\n"
+  print "HTTP response: \n"
+  print http_res
+
+  # Check for correct host and port
+  if "localhost:3000" in http_res:
+    print "Success! 1 out of 1 tests passed"
+  else:
+    print "Error, check output: \n" + http_res 
+
+  # Kill webserver
+  PROCNAME = "webserver"
+  for proc in psutil.process_iter():
+    if proc.name() == PROCNAME:
+      print "Webserver process found, kill process..."
+      proc.kill()
 
 if __name__ == "__main__":
   main()

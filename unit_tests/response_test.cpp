31db5ad9807f6d8064c1731c3857f54ed8189477
Zhengxu Xia <kevinxzx95@gmail.com>
2017-02-24 23:08:51 -0800

    Now response test case is good.

--- unit_tests/response_test.cpp
+++ unit_tests/response_test.cpp
@@ -3,21 +3,36 @@
 #include <string>
 #include <boost/lexical_cast.hpp>
 
-// // test fixture used in following test
-// class ResponseTest:public::testing::Test{
-// protected:
-//   response rply;
-//   response out;
-//   header header;
-// };
-//
-// // test stock_reply
-// TEST_F(ResponseTest, stock_reply_Test){
-//   out = rply.stock_reply(http::server::reply::ok);
-//   EXPECT_TRUE(out.status == 200);
-//   EXPECT_EQ(200, out.status);
-//   EXPECT_EQ("", out.content);
-//   EXPECT_EQ("Content-Length", out.headers[0].name);
-//   EXPECT_EQ("Content-Type", out.headers[1].name);
-//   EXPECT_EQ("text/html", out.headers[1].value);
-// }
+// test fixture used in following test
+class ResponseTest:public::testing::Test{
+protected:
+  Response* response;
+  void SetUp()
+  {
+  	response = new Response;
+  	response->SetStatus(Response::ok);
+    response->AddHeader("abc", "def");
+    response->SetBody("123456789");
+
+  }
+  void TearDown(){
+    delete response;
+  }
+};
+
+// test content_length
+TEST_F(ResponseTest, content_length_Test){
+  EXPECT_EQ(9, response->ContentLength());
+}
+
+// test Get_response_code
+TEST_F(ResponseTest, get_response_code_Test){
+  EXPECT_EQ("200", response->GetResponseCode());
+}
+
+// test to_string
+TEST_F(ResponseTest, tostring_Test){
+  std::string result = "HTTP/1.0 200 OK\r\nabc: def\r\n\r\n123456789";
+
+  EXPECT_EQ(result, response->ToString());
+}
